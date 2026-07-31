280a45ea219d60e87e07353adace22ee6db5c886
Rokoudram <merlin.olivry.34@gmail.com>
2026-07-31 13:52:27 +0200

    Suppression du base path /jeux-maths/

--- config/sections.ts
+++ config/sections.ts
@@ -8,6 +8,14 @@ export const sections = [
         nav_mobile:true,
     },
     {
+        title: "Exercices",
+        href: "/exercices",
+        description:
+        "Exercices de mathématiques niveau lycée",
+        nav: true,
+        nav_mobile:true,
+    },
+    {
         title: "À propos",
         href: "/about",
         description:
